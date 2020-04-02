7eb7adefe0e7492d4db27012cf0a6b26c39541f8
Vpsolo <1foreverfarever@gmail.com>
2020-04-02 21:07:43 +0600

    lesson 44

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,9 +6,20 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
+
 class IndexController extends Controller
 {
     public function execute(Request $request){
-      return view('layouts.site');
+      $pages = Page::all(); 
+      // $portfolio = Portfolio::all(); 
+      $portfolio = Portfolio::get(array('name','filter','images')); 
+      $services = Service::where('id','<',20)->get(); 
+      $people = People::take(3)->get();
+
+      return view('site.index');
     }
 }
