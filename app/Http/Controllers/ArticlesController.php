e891b1cdbc0fc40a9fb5b9e2faef6b1c5f33241b
Vpsolo <1foreverfarever@gmail.com>
2020-04-08 23:12:52 +0600

    lesson 68

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -6,45 +6,45 @@ use Illuminate\Http\Request;
 
 use Corp\Repositories\PortfoliosRepository;
 use Corp\Repositories\ArticlesRepository;
+use Corp\Repositories\CommentsRepository;
 
 use Corp\Http\Requests;
 
 class ArticlesController extends SiteController
 {
-  public function __construct(PortfoliosRepository $p_rep,ArticlesRepository $a_rep){
+  public function __construct(PortfoliosRepository $p_rep,ArticlesRepository $a_rep,CommentsRepository $c_rep){
     parent::__construct(new \Corp\Repositories\MenusRepository(new \Corp\Menu));
 
     $this->p_rep = $p_rep;
     $this->a_rep = $a_rep;
+    $this->c_rep = $c_rep;
 
     $this->bar = 'right';
     $this->template = env('THEME').'.articles';
   }
 
   public function index(){
-    // $portfolios = $this->getPortfolio();
-    // $content = view(env('THEME').'.content')->with('portfolios',$portfolios)->render();
-    // $this->vars = array_add($this->vars,'content',$content); 
-
-    // $slidersItems = $this->getSliders();
-    // $sliders = view(env('THEME').'.slider')->with('sliders',$slidersItems)->render();
-    // $this->vars = array_add($this->vars,'sliders',$sliders); 
- 
-    // $this->keywords = 'Home Page';
-    // $this->meta_desc = 'Home Page';
-    // $this->title = 'Home Page';
-
-    // $articles = $this->getArticles();
-    // $this->contentRightBar = view(env('THEME').'.indexBar')->with('articles',$articles)->render();
-
     $articles = $this->getArticles();
-    
-    $content =view(env('THEME').'.articles_content')->with('articles',$articles)->render();
+    $content = view(env('THEME').'.articles_content')->with('articles',$articles)->render();
     $this->vars = array_add($this->vars,'content',$content); 
+
+    $comments = $this->getComments(config('settings.recent_comments'));
+    $portfolios = $this->getPortfolios(config('settings.recent_portfolios'));
+    $this->contentRightBar = view(env('THEME').'.articlesBar')->with(['comments'=>$comments,'portfolios'=>$portfolios]);
 
     return $this->renderOutput();
   }
-  
+
+  public function getComments($take){
+    $comments = $this->c_rep->get(['text','name','email','site','article_id','user_id'],$take);
+    return $comments;
+  }
+
+  public function getPortfolios($take){
+    $portfolios = $this->p_rep->get(['title','text','alias','customer','img','filter_alias'],$take);
+    return $portfolios;
+  }
+
   public function getArticles($alias = FALSE){
     $articles = $this->a_rep->get(['id','title','alias','created_at','img','desc','user_id','category_id'],FALSE,TRUE);
 
--- config/settings.php
+++ config/settings.php
@@ -4,6 +4,8 @@
     'home_port_count' => 5,
     'home_articles_count' => 3,
     'paginate' => 2,
-    'read_more' => ' Читать далее',
+    'read_more' => 'Читать далее',
+    'recent_comments' => 3,
+    'recent_portfolios' => 3,
   ];
 ?>
