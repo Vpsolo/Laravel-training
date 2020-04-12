14bea0095ed7c0e0b7661b05b5de3879a44403be
Vpsolo <1foreverfarever@gmail.com>
2020-04-12 10:38:08 +0600

    lesson 81

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,11 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+
+    protected $loginView;
+    protected $username = 'login';
+
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new authentication controller instance.
@@ -38,6 +42,21 @@ class AuthController extends Controller
     public function __construct()
     {
         $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+      
+      $this->loginView = env('THEME').'.login';
+    }
+
+    public function showLoginForm()
+    {
+        $view = property_exists($this, 'loginView')
+                    ? $this->loginView : '';
+
+        if (view()->exists($view)) {
+            return view($view)->with('title','Вход на сайт');
+        }
+        
+        abort(404);
+        // return view('auth.login');
     }
 
     /**
