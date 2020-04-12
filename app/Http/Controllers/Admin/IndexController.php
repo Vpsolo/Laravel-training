c240c6e9f0146b85993ee0dcdcc2982e84ea7171
Vpsolo <1foreverfarever@gmail.com>
2020-04-13 00:53:53 +0600

    lesson 84

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -7,10 +7,16 @@ use Illuminate\Http\Request;
 use Corp\Http\Requests;
 use Corp\Http\Controllers\Controller;
 
+use Gate;
+
 class IndexController extends AdminController
 {
   public function __construct(){
     parent::__construct();
+
+    if(Gate::denies('VIEW_ADMIN')){
+      abort(403);
+    }
 
     $this->template = env('THEME').'.admin.index';
   }
--- app/User.php
+++ app/User.php
@@ -24,11 +24,55 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function articles(){
-      return $this->hasMany('Corp\Article');
-    }
+  public function articles(){
+    return $this->hasMany('Corp\Article');
+  }
 
-    public function roles(){
-      return $this->belongsToMany('Corp\Role','role_user');
+  public function roles(){
+    return $this->belongsToMany('Corp\Role','role_user');
+  }
+  
+  public function canDo($permission,$require=FALSE){
+    if (is_array($permission)) {
+      foreach($permission as $permName) {
+        $permName = $this->canDo($permName);
+        if ($permName && !$require) {
+          return TRUE;
+        } else if (!$permName && $require) {
+          return FALSE;
+        }
+      }
+      return $require;
+    } else {
+      foreach($this->roles as $role) {
+        foreach($role->perms as $perm) {
+          if (str_is($permission, $perm->name)) {
+            return TRUE;
+          }
+        }
+      }
     }
+    return FALSE;
+  }
+
+  public function hasRole($permission,$require=FALSE){
+    if (is_array($name)) {
+      foreach($name as $roleName) {
+        $hasRole = $this->hasRole($roleName);
+        if ($hasRole && !$require) {
+          return TRUE;
+        } else if (!$hasRole && $require) {
+          return FALSE;
+        }
+      }
+      return $require;
+    } else {
+      foreach($this->roles as $role) {
+        if ($role->name == $name) {
+          return TRUE;
+        }
+      }
+    }
+    return FALSE;
+  }
 }
