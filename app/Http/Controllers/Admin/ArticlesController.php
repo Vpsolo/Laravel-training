6e03bc873e472329c78722e65f75a277e524fd4c
Vpsolo <1foreverfarever@gmail.com>
2020-04-13 14:04:01 +0600

    lesson 86

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -11,6 +11,8 @@ use Corp\Repositories\ArticlesRepository;
 
 use Gate;
 
+use Corp\Category;
+
 class ArticlesController extends AdminController
 {
   public function __construct(ArticlesRepository $a_rep){
@@ -55,7 +57,26 @@ class ArticlesController extends AdminController
      */
     public function create()
     {
-        //
+      if(Gate::denies('save',new \Corp\Article)){
+        abort(403);
+      }
+
+      $this->title = 'Добавить новый материал';
+
+      // Можно добавить репозиторий для категорий
+      $categories = Category::select(['title','alias','parent_id','id'])->get();
+      $lists = array();
+      foreach($categories as $category){
+        if($category->parent_id == 0){
+          $lists[$category->title] = array();
+        }else{
+          $lists[$category->where('id',$category->parent_id)->first()->title][$category->id] = $category->title;
+        }
+      }
+    
+      $this->content = view(env('THEME').'.admin.articles_create_content')->with('categories',$lists)->render();
+      
+      return $this->renderOutput();
     }
 
     /**
